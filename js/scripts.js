9a827cf82ef348017d4d90a246be7580b11f1fb6
sleepsim <91400544+sleepsim@users.noreply.github.com>
2023-03-30 04:20:14 -0700

    about page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 // Mouse over events mainly from: https://www.w3schools.com/jsref/event_onmouseover.asp
-function stylize(item){
+function stylizeLink(item){
     var holder = item.innerHTML;
     item.innerHTML = ">" + holder;
 }
 
-function restore(item){
+function restoreLink(item){
     var holder = item.innerHTML;
     var edited = holder.slice(4);
     item.innerHTML = edited;
